00a208cc3ac2b6d69ab603ce63fde9384dfe37dc
marykamau2 <mary.kamau@student.moringaschool.com>
2021-06-14 13:50:14 +0300

    added a background color

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 function output() {
+  
+
+    
     var day = document.getElementById("day").value;
     var dd = parseInt(day);
     var month = document.getElementById("month").value;
     var mm = parseInt(month);
     var year = document.getElementById("year").value;
     var yy = parseInt(year);
-     var cc = (yy - 1) / 100 + 1;
+    var cc = (yy - 1) / 100 + 1;
     var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
     var boy = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var girl = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
@@ -80,4 +83,4 @@ function output() {
    else {
     alert("Enter your information please");
     }   
-    }
+}
