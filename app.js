a20667ac8f68bc8297ecbda3d0d1de8a67531e81
marykamau2 <mary.kamau@student.moringaschool.com>
2021-06-14 13:12:37 +0300

    assigned the day of the week

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function output() {
     var mm = parseInt(month);
     var year = document.getElementById("year").value;
     var yy = parseInt(year);
-    var cc = (yy - 1) / 100 + 1;
+   /*  var cc = (yy - 1) / 100 + 1;
     var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
     var boy = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var girl = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
@@ -79,5 +79,5 @@ function output() {
     }
    else {
     alert("enter your information please");
-    }
+    }  */
     }
